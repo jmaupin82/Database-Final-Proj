a91c050f53115dc1c988b9a1782c144c9653bb04
Ali Hajimirza <a92hm@yahoo.com>
2013-11-19 16:12:13 -0600

    ui

--- Driver/Driver.java
+++ Driver/Driver.java
@@ -1,5 +1,37 @@
 package Driver;
 
-public class Driver {
+import View.View;
+import Controller.DBController;
+import Model.DBModel;
+
+/**
+ * Class Driver
+ * Runs the gui application
+ */
+public class Driver 
+{
+    private DBModel jobShopModel;
+    private View jobShopView;
+    private DBController jobShopController;
+    
+    public Driver()
+    {
+	this.jobShopModel = new DBModel();
+	this.jobShopController = new DBController();
+	this.jobShopView = new View();
+	
+	this.jobShopController.setDBModel(this.jobShopModel);
+	this.jobShopController.setView(this.jobShopView);
+	this.jobShopView.setDBModel(this.jobShopModel);
+    }
+    
+    /**
+     * Main function
+     * @param args
+     */
+    public static void main (String [] args)
+    {
+	new Driver();
+    }
 
 }
